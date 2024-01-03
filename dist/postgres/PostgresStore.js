f22a7670ce0fefcd645dd7ec0d023109bbd09c79
Joel <git@runningant.com>
2024-01-04 10:56:02 +1100

    Reading connection settings from .env

--- dist/postgres/PostgresStore.js
+++ dist/postgres/PostgresStore.js
@@ -3,18 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PostgresStore = void 0;
 const PostgresDB_1 = require("./PostgresDB");
 const SqlStore_1 = require("../common/SqlStore");
+const rant_utils_1 = require("rant-utils");
 class PostgresStore extends SqlStore_1.SqlStore {
     constructor() {
         super();
     }
     async connect() {
+        const env = process.env;
         const db = new PostgresDB_1.PostgresDB({
-            host: "localhost",
-            port: 5432,
-            user: "postgres",
-            password: "postgres",
-            database: "store",
-            ssl: false,
+            host: env.DB_HOST,
+            port: (0, rant_utils_1.toInt)(env.DB_PORT),
+            user: env.DB_USER,
+            password: env.DB_PASSWORD,
+            database: env.DB_DATABASE,
+            ssl: (env.DB_SSL ? true : false),
         });
         await db.connect();
         this.db = db;
